358ed5f9120dc4e59b0f2bccad3d4998601d4679
Shaopeng Jia <jia.shao.peng@gmail.com>
2010-09-06 13:32:32 +0000

    Minor documentation updates.

--- java/src/com/google/i18n/phonenumbers/AsYouTypeFormatter.java
+++ java/src/com/google/i18n/phonenumbers/AsYouTypeFormatter.java
@@ -323,9 +323,9 @@ public class AsYouTypeFormatter {
     if (!ableToFormat) {
       return originalPosition;
     }
-    int accruedInputIndex = 0, currentOutputIndex = 0;
+    int accruedInputIndex = 0;
+    int currentOutputIndex = 0;
     int currentOutputLength = currentOutput.length();
-    
     while (accruedInputIndex < positionToRemember && currentOutputIndex < currentOutputLength) {
       if (accruedInputWithoutFormatting.charAt(accruedInputIndex) ==
           currentOutput.charAt(currentOutputIndex)) {
