a6b4b1b91586507c1f2de4a58cb36a9b1101e8c6
Keghani Kouzoujian <keghani@gmail.com>
2015-10-07 17:40:01 +0200

    Make robust SingleFileMetadataSourceImpl and make it consistent with MultiFileMetadataSourceImpl.

--- java/libphonenumber/test/com/google/i18n/phonenumbers/SingleFileMetadataSourceImplTest.java
+++ java/libphonenumber/test/com/google/i18n/phonenumbers/SingleFileMetadataSourceImplTest.java
@@ -19,29 +19,22 @@ package com.google.i18n.phonenumbers;
 import junit.framework.TestCase;
 
 /**
- * Unit tests for MultiFileMetadataSourceImpl.java.
+ * Unit tests for SingleFileMetadataSourceImpl.java.
  */
-public class MultiFileMetadataSourceImplTest extends TestCase {
-  public MultiFileMetadataSourceImplTest() {}
+public class SingleFileMetadataSourceImplTest extends TestCase {
+  public SingleFileMetadataSourceImplTest() {}
 
   public void testMissingMetadataFileThrowsRuntimeException() {
-    MultiFileMetadataSourceImpl multiFileMetadataSource = new MultiFileMetadataSourceImpl(
+    SingleFileMetadataSourceImpl singleFileMetadataSource = new SingleFileMetadataSourceImpl(
         "no/such/file", PhoneNumberUtil.DEFAULT_METADATA_LOADER);
     // In normal usage we should never get a state where we are asking to load metadata that doesn't
     // exist. However if the library is packaged incorrectly in the jar, this could happen and the
     // best we can do is make sure the exception has the file name in it.
     try {
-      multiFileMetadataSource.loadMetadataFromFile("XX", -1);
+      singleFileMetadataSource.loadMetadataFromFile();
       fail("expected exception");
     } catch (RuntimeException e) {
-      assertTrue("Unexpected error: " + e, e.getMessage().contains("no/such/file_XX"));
-    }
-    try {
-      multiFileMetadataSource.loadMetadataFromFile(
-          PhoneNumberUtil.REGION_CODE_FOR_NON_GEO_ENTITY, 123);
-      fail("expected exception");
-    } catch (RuntimeException e) {
-      assertTrue("Unexpected error: " + e, e.getMessage().contains("no/such/file_123"));
+      assertTrue("Unexpected error: " + e, e.getMessage().contains("no/such/file"));
     }
   }
 }
