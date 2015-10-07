e9b82df4c25ba65d945565c241336d433d8175be
Keghani Kouzoujian <keghani@gmail.com>
2015-10-07 10:49:47 +0200

    Use instance variables rather than passing them through methods in MultiFileMetadataSourceImpl.

--- java/libphonenumber/test/com/google/i18n/phonenumbers/MultiFileMetadataSourceImplTest.java
+++ java/libphonenumber/test/com/google/i18n/phonenumbers/MultiFileMetadataSourceImplTest.java
@@ -24,8 +24,8 @@ public class MultiFileMetadataSourceImplTest extends TestMetadataTestCase  {
   private final MultiFileMetadataSourceImpl multiFileMetadataSource;
 
   public MultiFileMetadataSourceImplTest() {
-    multiFileMetadataSource = new MultiFileMetadataSourceImpl(TEST_META_DATA_FILE_PREFIX,
-        PhoneNumberUtil.DEFAULT_METADATA_LOADER);
+    multiFileMetadataSource = new MultiFileMetadataSourceImpl(
+        "no/such/file", PhoneNumberUtil.DEFAULT_METADATA_LOADER);
   }
 
   public void testMissingMetadataFileThrowsRuntimeException() {
@@ -33,16 +33,14 @@ public class MultiFileMetadataSourceImplTest extends TestMetadataTestCase  {
     // exist. However if the library is packaged incorrectly in the jar, this could happen and the
     // best we can do is make sure the exception has the file name in it.
     try {
-      multiFileMetadataSource.loadMetadataFromFile(
-          "no/such/file", "XX", -1, PhoneNumberUtil.DEFAULT_METADATA_LOADER);
+      multiFileMetadataSource.loadMetadataFromFile("XX", -1);
       fail("expected exception");
     } catch (RuntimeException e) {
       assertTrue("Unexpected error: " + e, e.toString().contains("no/such/file_XX"));
     }
     try {
-      multiFileMetadataSource.loadMetadataFromFile("no/such/file",
-          PhoneNumberUtil.REGION_CODE_FOR_NON_GEO_ENTITY, 123,
-          PhoneNumberUtil.DEFAULT_METADATA_LOADER);
+      multiFileMetadataSource.loadMetadataFromFile(
+          PhoneNumberUtil.REGION_CODE_FOR_NON_GEO_ENTITY, 123);
       fail("expected exception");
     } catch (RuntimeException e) {
       assertTrue("Unexpected error: " + e, e.getMessage().contains("no/such/file_123"));
