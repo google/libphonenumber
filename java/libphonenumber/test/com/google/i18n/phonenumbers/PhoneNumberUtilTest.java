1bbbcf9b5147db63cfabb86cd6f49c4cb35ea7d7
Tiago Salem Herrmann <tiagosh@gmail.com>
2016-10-28 19:12:28 -0200

    Add hash as diallable character to java and javascript implementations

--- java/libphonenumber/test/com/google/i18n/phonenumbers/PhoneNumberUtilTest.java
+++ java/libphonenumber/test/com/google/i18n/phonenumbers/PhoneNumberUtilTest.java
@@ -427,7 +427,7 @@ public class PhoneNumberUtilTest extends TestMetadataTestCase {
 
   public void testNormaliseStripNonDiallableCharacters() {
     String inputNumber = "03*4-56&+a#234";
-    String expectedOutput = "03*456+234";
+    String expectedOutput = "03*456+#234";
     assertEquals("Conversion did not correctly remove non-diallable characters",
                  expectedOutput,
                  PhoneNumberUtil.normalizeDiallableCharsOnly(inputNumber));
