348b363eef7b2eed958e69965869b06204f8cc76
Aravind <penmetsaa@google.com>
2017-01-25 06:08:11 +0530

    Metadata updates for release 8.1.0

--- java/libphonenumber/src/com/google/i18n/phonenumbers/AlternateFormatsCountryCodeSet.java
+++ java/libphonenumber/src/com/google/i18n/phonenumbers/AlternateFormatsCountryCodeSet.java
@@ -26,9 +26,9 @@ import java.util.Set;
 public class AlternateFormatsCountryCodeSet {
   // A set of all country codes for which data is available.
   static Set<Integer> getCountryCodeSet() {
-    // The capacity is set to 57 as there are 43 different entries,
+    // The capacity is set to 58 as there are 44 different entries,
     // and this offers a load factor of roughly 0.75.
-    Set<Integer> countryCodeSet = new HashSet<Integer>(57);
+    Set<Integer> countryCodeSet = new HashSet<Integer>(58);
 
     countryCodeSet.add(7);
     countryCodeSet.add(27);
@@ -39,6 +39,7 @@ public class AlternateFormatsCountryCodeSet {
     countryCodeSet.add(43);
     countryCodeSet.add(44);
     countryCodeSet.add(49);
+    countryCodeSet.add(52);
     countryCodeSet.add(54);
     countryCodeSet.add(55);
     countryCodeSet.add(58);
