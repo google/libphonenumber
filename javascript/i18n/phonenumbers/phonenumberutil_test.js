4cdd9ddc136c11310a8bb9ff16a090714f4f1f13
Andy Staudacher <andy.st@gmail.com>
2017-08-07 15:15:16 +0200

    Update phonenumberutil_test.js

--- javascript/i18n/phonenumbers/phonenumberutil_test.js
+++ javascript/i18n/phonenumbers/phonenumberutil_test.js
@@ -2760,8 +2760,8 @@ function testParseNationalNumber() {
   shortNumber.setNationalNumber(12);
   assertTrue(shortNumber.equals(phoneUtil.parse('12', RegionCode.NZ)));
 
-  // Test for b/64235606. short-code with leading zero for a country which has 0
-  // as national prefix. Ensure it's not interpreted as national prefix if the
+  // Test for short-code with leading zero for a country which has 0 as
+  // national prefix. Ensure it's not interpreted as national prefix if the
   // remaining number length is local-only in terms of length. Example: In GB,
   // length 6-7 are only possible local-only.
   shortNumber.setCountryCode(44);
