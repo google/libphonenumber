ca2951dd9c985a71f73328524994ee01ae05554f
Tomiwa Oke <tomiwaoke9@gmail.com>
2020-07-21 15:52:30 +0000

    changes based on PR comments

---
+++ migrator/src/main/java/com/google/phonenumbers/migrator/MetadataZipFileReader.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2020 The Libphonenumber Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.phonenumbers.migrator;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.i18n.phonenumbers.metadata.model.RangesTableSchema;
+import com.google.i18n.phonenumbers.metadata.table.CsvTable;
+import com.google.i18n.phonenumbers.metadata.table.RangeKey;
+import com.google.common.base.Preconditions;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ * Represents a standard compliant metadata zip file where each {@link MetadataZipFileReader} zip
+ * file contains {@link CsvTable}'s that can be imported based on specified BCP47 version region
+ * codes without the need to extract the whole zip file.
+ */
+public final class MetadataZipFileReader {
+
+  private final ZipFile metadataZipFile;
+  private final String zipName;
+
+  private MetadataZipFileReader(String fileLocation, String zipName) throws IOException {
+    this.metadataZipFile = new ZipFile(fileLocation);
+    this.zipName = zipName;
+  }
+
+  /**
+   * Returns a MetadataZipFileReader for the given string (e.g. "./metadata.zip").
+   */
+  public static MetadataZipFileReader of(String fileLocation) throws IOException {
+    Preconditions.checkNotNull(fileLocation);
+    Preconditions.checkArgument(new File(fileLocation).exists(),
+        "Invalid zip file location: ", fileLocation);
+
+    String[] dirPaths = fileLocation.split("/");
+    String zipName = dirPaths[dirPaths.length - 1].isEmpty() ?
+        dirPaths[dirPaths.length - 2] : dirPaths[dirPaths.length - 1];
+    return new MetadataZipFileReader(fileLocation, zipName.split(".zip")[0]);
+  }
+
+  /**
+   * Returns the {@link CsvTable} for the given BCP47 region code (e.g. "44") if present.
+   */
+  public Optional<CsvTable<RangeKey>> importCsvTable(String regionCode) throws IOException {
+    String csvTableLocation = zipName + "/" + regionCode + "/ranges.csv";
+    ZipEntry csvFile = metadataZipFile.getEntry(csvTableLocation);
+
+    if (csvFile == null) {
+      return Optional.empty();
+    }
+    return Optional.of(CsvTable.importCsv(RangesTableSchema.SCHEMA,
+        new InputStreamReader(metadataZipFile.getInputStream(csvFile))));
+  }
+
+  public static void main(String[] args) throws Exception {
+    String fileLocation;
+    String regionCode;
+
+    if (args.length < 2) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("You have not entered correct command line arguments");
+
+      System.out.print("\tPlease enter a zip file location of metadata csv files: ");
+      fileLocation = scanner.next();
+
+      System.out.print("\tPlease enter the region code of the ranges you want to import: ");
+      regionCode = scanner.next();
+    } else {
+      fileLocation = args[0];
+      regionCode = args[1];
+    }
+
+    MetadataZipFileReader m = MetadataZipFileReader.of(fileLocation);
+    Optional<CsvTable<RangeKey>> ranges = m.importCsvTable(regionCode);
+    ranges.orElseThrow(() -> new Exception("Region code not supported in zipfile"));
+
+    System.out.println("Table imported!");
+    ImmutableSet<RangeKey> columns = ranges.get().getKeys();
+    for (RangeKey key : columns) {
+      System.out.println(key);
+    }
+  }
+
+}
---
+++ migrator/src/test/java/com/google/phonenumbers/migrator/MetadataZipFileReaderTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2020 The Libphonenumber Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.phonenumbers.migrator;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.phonenumbers.migrator.testing.testUtils.AssertUtil.assertThrows;
+
+import com.google.i18n.phonenumbers.metadata.table.CsvTable;
+import com.google.i18n.phonenumbers.metadata.table.RangeKey;
+import java.io.IOException;
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class MetadataZipFileReaderTest {
+  private String testDataPath = "./src/test/java/com/google/phonenumbers/migrator/testing/testData/";
+
+  @Test
+  public void testInvalidFileLocation() {
+    String fileLocation = "invalid-zipfile-location";
+    assertThrows(IllegalArgumentException.class, () -> {
+      try {
+        MetadataZipFileReader.of(fileLocation);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    });
+  }
+
+  @Test
+  public void testValidCsvTable() throws IOException {
+    String fileLocation = testDataPath + "valid-metadata-structure.zip";
+    MetadataZipFileReader validZip = MetadataZipFileReader.of(fileLocation);
+    Optional<CsvTable<RangeKey>> regionTable = validZip.importCsvTable("1");
+    assertThat(regionTable.isPresent()).isEqualTo(true);
+  }
+
+  @Test
+  public void testInvalidCsvTable() throws IOException {
+    String fileLocation = testDataPath + "invalid-metadata-structure.zip/";
+    MetadataZipFileReader validZip = MetadataZipFileReader.of(fileLocation);
+    Optional<CsvTable<RangeKey>> regionTable = validZip.importCsvTable("1");
+    assertThat(regionTable.isPresent()).isEqualTo(false);
+  }
+}
