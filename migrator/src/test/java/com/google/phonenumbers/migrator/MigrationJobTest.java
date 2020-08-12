d75f5352b58ef0f3970a63fb486a370d206587a7
Tomiwa Oke <tomiwaoke9@gmail.com>
2020-08-12 09:28:59 +0000

    test updates

--- migrator/src/test/java/com/google/phonenumbers/migrator/MigrationJobTest.java
+++ migrator/src/test/java/com/google/phonenumbers/migrator/MigrationJobTest.java
@@ -1,20 +1,19 @@
 package com.google.phonenumbers.migrator;
 
-    import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertThat;
 
-    import com.google.i18n.phonenumbers.metadata.DigitSequence;
-    import com.google.i18n.phonenumbers.metadata.RangeSpecification;
-    import com.google.i18n.phonenumbers.metadata.RangeTree;
-    import com.google.i18n.phonenumbers.metadata.table.RangeKey;
-    import java.io.FileNotFoundException;
-    import java.io.IOException;
-    import java.nio.file.NoSuchFileException;
-    import java.nio.file.Paths;
-    import java.util.Collections;
-    import org.junit.Assert;
-    import org.junit.Test;
-    import org.junit.runner.RunWith;
-    import org.junit.runners.JUnit4;
+import com.google.i18n.phonenumbers.metadata.DigitSequence;
+import com.google.i18n.phonenumbers.metadata.RangeSpecification;
+import com.google.i18n.phonenumbers.metadata.RangeTree;
+import com.google.i18n.phonenumbers.metadata.table.RangeKey;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.Collections;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class MigrationJobTest {
@@ -22,23 +21,23 @@ public class MigrationJobTest {
   private static final String TEST_DATA_PATH = "./src/test/java/com/google/phonenumbers/migrator/testing/testData/";
 
   @Test
-  public void testInvalidNumbersFile() {
+  public void createFromFilePath_invalidPathLocation_expectException() {
     String fileLocation = "invalid-path-location";
     try {
-      MigrationJob.from(Paths.get(fileLocation), "GB");
+      MigrationFactory.createMigration(Paths.get(fileLocation), "GB");
       Assert.fail("Expected IOException and did not receive");
     } catch (IOException e) {
-      assertThat(e).isInstanceOf(FileNotFoundException.class);
+      assertThat(e).isInstanceOf(NoSuchFileException.class);
       assertThat(e).hasMessageThat().contains(fileLocation);
     }
   }
 
   @Test
-  public void testInvalidNumberString() {
+  public void createFromNumberString_invalidNumberFormat_expectException() {
     String numberInput = "+44 one2 34 56";
     String sanitizedNumber = "44one23456";
     try {
-      MigrationJob.from(numberInput, "GB");
+      MigrationFactory.createMigration(numberInput, "GB");
       Assert.fail("Expected RuntimeException and did not receive");
     } catch (RuntimeException e) {
       assertThat(e).isInstanceOf(IllegalArgumentException.class);
@@ -51,10 +50,10 @@ public class MigrationJobTest {
   }
 
   @Test
-  public void testInvalidCustomRecipesPath() {
+  public void createWithCustomRecipes_invalidPathLocation_expectException() {
     String fileLocation = "invalid-recipe-location";
     try {
-      MigrationJob.from("12345", "GB", fileLocation);
+      MigrationFactory.createMigration("12345", "GB", Paths.get(fileLocation));
       Assert.fail("Expected IOException and did not receive");
     } catch (IOException e) {
       assertThat(e).isInstanceOf(NoSuchFileException.class);
@@ -63,19 +62,20 @@ public class MigrationJobTest {
   }
 
   @Test
-  public void testGetAllMigratableNumbersWithNoMatches() throws IOException {
+  public void getAllMigratableNumbers_expectNoMatches() throws IOException {
     String recipesPath = TEST_DATA_PATH + "testRecipesFile.csv";
-    MigrationJob job = MigrationJob.from("34", "GB", recipesPath);
+    MigrationJob job = MigrationFactory.createMigration("34", "GB", Paths.get(recipesPath));
 
     RangeTree noMatchesRange = job.getAllMigratableNumbers();
     assertThat(noMatchesRange.asRangeSpecifications()).isEmpty();
   }
 
   @Test
-  public void testGetAllMigratableNumbersWithMatches() throws IOException {
+  public void getAllMigratableNumbers_expectMatches() throws IOException {
     String recipesPath = TEST_DATA_PATH + "testRecipesFile.csv";
     String numbersPath = TEST_DATA_PATH + "testNumbersFile.txt";
-    MigrationJob job = MigrationJob.from(Paths.get(numbersPath), "GB", recipesPath);
+    MigrationJob job = MigrationFactory
+        .createMigration(Paths.get(numbersPath), "GB", Paths.get(recipesPath));
 
     RangeTree noMatchesRange = job.getAllMigratableNumbers();
     assertThat(noMatchesRange.asRangeSpecifications())
@@ -83,12 +83,12 @@ public class MigrationJobTest {
   }
 
   @Test
-  public void testGetMigratableNumbersWithInvalidKey() throws IOException {
+  public void getMigratableNumbers_invalidKey_expectException() throws IOException {
     String recipesPath = TEST_DATA_PATH + "testRecipesFile.csv";
     RangeSpecification testRangeSpec = RangeSpecification.from(DigitSequence.of("123"));
     RangeKey invalidKey = RangeKey.create(testRangeSpec, Collections.singleton(3));
 
-    MigrationJob job = MigrationJob.from("123", "GB", recipesPath);
+    MigrationJob job = MigrationFactory.createMigration("123", "GB", Paths.get(recipesPath));
     try {
       job.getMigratableNumbers(invalidKey);
       Assert.fail("Expected RuntimeException and did not receive");
@@ -99,12 +99,12 @@ public class MigrationJobTest {
   }
 
   @Test
-  public void testGetMigratableNumbersWithValidKey() throws IOException {
+  public void getMigratableNumbers_validKey_expectNoExceptionAndNoMatches() throws IOException {
     String recipesPath = TEST_DATA_PATH + "testRecipesFile.csv";
     RangeSpecification testRangeSpec = RangeSpecification.from(DigitSequence.of("12"));
     RangeKey validKey = RangeKey.create(testRangeSpec, Collections.singleton(5));
 
-    MigrationJob job = MigrationJob.from("123", "GB", recipesPath);
+    MigrationJob job = MigrationFactory.createMigration("123", "GB", Paths.get(recipesPath));
     assertThat(job.getMigratableNumbers(validKey).asRangeSpecifications()).isEmpty();
   }
 }
