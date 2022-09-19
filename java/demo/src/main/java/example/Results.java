feee7d5b1a4854e84dd1d7dbf36b343801ff25e9
26879902+roggnroll@users.noreply.github.com <26879902+roggnroll@users.noreply.github.com>
2022-09-19 15:27:10 +0200

    result page

---
+++ java/demo/src/main/java/example/Results.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2011 The Libphonenumber Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Shaopeng Jia
+ */
+
+package example;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.template.soy.SoyFileSet;
+import com.google.template.soy.tofu.SoyTofu;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.util.Streams;
+import org.apache.commons.io.IOUtils;
+
+/**
+ * A servlet that accepts requests that contain strings representing a phone number and a default
+ * country, and responds with results from parsing, validating and formatting the number. The
+ * default country is a two-letter region code representing the country that we are expecting the
+ * number to be from.
+ */
+@SuppressWarnings("serial")
+public class Results extends HttpServlet {
+
+
+  /** Handle the get request to get information about a number based on query parameters. */
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    resp.setContentType("text/html");
+    resp.setCharacterEncoding(UTF_8.name());
+    SoyFileSet sfs = SoyFileSet
+        .builder()
+        .add(Results.class.getResource("results.soy"))
+        .build();
+    SoyTofu tofu = sfs.compileToTofu();
+    resp.getWriter()
+        .println(
+            tofu.newRenderer("examples.simple.results").render());
+  }
+
+
+  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String phoneNumber = null;
+    String defaultCountry = null;
+    String languageCode = "en"; // Default languageCode to English if nothing is entered.
+    String regionCode = "";
+    String fileContents = null;
+    ServletFileUpload upload = new ServletFileUpload();
+    upload.setSizeMax(50000);
+    try {
+      FileItemIterator iterator = upload.getItemIterator(req);
+      while (iterator.hasNext()) {
+        FileItemStream item = iterator.next();
+        InputStream in = item.openStream();
+        if (item.isFormField()) {
+          String fieldName = item.getFieldName();
+          if (fieldName.equals("phoneNumber")) {
+            phoneNumber = Streams.asString(in, UTF_8.name());
+          } else if (fieldName.equals("defaultCountry")) {
+            defaultCountry = Streams.asString(in).toUpperCase();
+          } else if (fieldName.equals("languageCode")) {
+            String languageEntered = Streams.asString(in).toLowerCase();
+            if (languageEntered.length() > 0) {
+              languageCode = languageEntered;
+            }
+          } else if (fieldName.equals("regionCode")) {
+            regionCode = Streams.asString(in).toUpperCase();
+          }
+        } else {
+          try {
+            fileContents = IOUtils.toString(in);
+          } finally {
+            IOUtils.closeQuietly(in);
+          }
+        }
+      }
+    } catch (FileUploadException e1) {
+      e1.printStackTrace();
+    }
+
+    StringBuilder output;
+    resp.setContentType("text/html");
+    resp.setCharacterEncoding(UTF_8.name());
+
+
+    resp.setContentType("text/html");
+    resp.setCharacterEncoding(UTF_8.name());
+    SoyFileSet sfs = SoyFileSet
+        .builder()
+        .add(Results.class.getResource("result.soy"))
+        .build();
+    SoyTofu tofu = sfs.compileToTofu();
+    resp.getWriter()
+        .println(
+            tofu.newRenderer("examples.simple.result").render());
+  }
+
+}
