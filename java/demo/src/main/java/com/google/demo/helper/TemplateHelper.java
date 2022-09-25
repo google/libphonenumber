f5ac44b6e77b29a1950380eb6e44324e160dc490
26879902+roggnroll@users.noreply.github.com <26879902+roggnroll@users.noreply.github.com>
2022-09-25 13:44:16 +0200

    Template Helper

--- java/demo/src/main/java/com/google/demo/helper/TemplateHelper.java
+++ java/demo/src/main/java/com/google/demo/helper/TemplateHelper.java
@@ -3,18 +3,22 @@ package com.google.demo.helper;
 import com.google.template.soy.SoyFileSet;
 import com.google.template.soy.data.SoyTemplate;
 import com.google.template.soy.tofu.SoyTofu;
+import java.net.URL;
 
 public class TemplateHelper {
 
   public static String templateToString(
       String template, String namespace, SoyTemplate soyTemplate) {
-    SoyFileSet sfs =
-        SoyFileSet.builder().add(TemplateHelper.class.getResource("../" + template)).build();
+    SoyFileSet sfs = SoyFileSet.builder().add(getResource(template)).build();
     SoyTofu tofu = sfs.compileToTofu();
     // For convenience, create another SoyTofu object that has a
     // namespace specified, so you can pass partial template names to
     // the newRenderer() method.
     SoyTofu simpleTofu = tofu.forNamespace(namespace);
     return simpleTofu.newRenderer(soyTemplate).render();
+  }
+
+  private static URL getResource(String template) {
+    return TemplateHelper.class.getResource("../" + template);
   }
 }
