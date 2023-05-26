75e2c493d9732c5487b45987de34a3ebe6e8802b
Meekness Adesina <zenon8adams@gmail.com>
2023-05-26 19:51:22 +0100

    Source: Replacement of StrCat and StrAppend with template equivalent
    - Removal of overload set of StrCat with templated version
    - Removal of overload set of StrAppend with templated version

--- cpp/src/phonenumbers/stringutil.cc
+++ cpp/src/phonenumbers/stringutil.cc
@@ -14,16 +14,15 @@
 
 // Author: Philippe Liard
 
-#include <algorithm>
-#include <cassert>
-#include <cstring>
-#include <sstream>
-
 #include "phonenumbers/stringutil.h"
 
+#include <algorithm>
+#include <cassert>
+#include <sstream>
+
+#include "absl/strings/match.h"
 #include "absl/strings/str_replace.h"
 #include "absl/strings/substitute.h"
-#include "absl/strings/match.h"
 
 namespace i18n {
 namespace phonenumbers {
@@ -31,50 +30,40 @@ namespace phonenumbers {
 using std::equal;
 using std::stringstream;
 
-string operator+(const string& s, int n) {  // NOLINT(runtime/string)
+string operator+(const string &s, int n) {  // NOLINT(runtime/string)
   string result;
-  absl::StrAppend(&result,s,n);
+  absl::StrAppend(&result, s, n);
   return result;
 }
 
-string SimpleItoa(int n) {
-  return absl::StrCat(n);
-}
+string SimpleItoa(int n) { return absl::StrCat(n); }
 
-string SimpleItoa(uint64 n) {
-  return absl::StrCat(n);
-}
+string SimpleItoa(uint64 n) { return absl::StrCat(n); }
 
-string SimpleItoa(int64 n) {
-  return absl::StrCat(n);
-}
+string SimpleItoa(int64 n) { return absl::StrCat(n); }
 
-bool HasPrefixString(const string& s, const string& prefix) {
+bool HasPrefixString(const string &s, const string &prefix) {
   return absl::StartsWith(s, prefix);
 }
 
-size_t FindNth(const string& s, char c, int n) {
-  size_t pos = string::npos;
-
-  for (int i = 0; i < n; ++i) {
-    pos = s.find_first_of(c, pos + 1);
-    if (pos == string::npos) {
-      break;
-    }
+size_t FindNth(const string &s, char c, int n) {
+  auto it = std::find_if(s.begin(), s.end(),
+                         [&](char val) { return val == c && --n == 0; });
+  if (it != s.end()) {
+    return std::distance(s.begin(), it);
   }
-  return pos;
+  return std::string::npos;
 }
 
-void SplitStringUsing(const string& s, char delimiter,
-                      vector<string>* result) {
+void SplitStringUsing(const string &s, char delimiter, vector<string> *result) {
   assert(result);
-  for (absl::string_view split_piece : absl::StrSplit(
-           s, absl::ByChar(delimiter), absl::SkipEmpty())) {
-    result->push_back(std::string(split_piece));
+  for (absl::string_view split_piece :
+       absl::StrSplit(s, absl::ByChar(delimiter), absl::SkipEmpty())) {
+    result->emplace_back(split_piece);
   }
 }
 
-bool TryStripPrefixString(const string& in, const string& prefix, string* out) {
+bool TryStripPrefixString(const string &in, const string &prefix, string *out) {
   assert(out);
   const bool has_prefix = in.compare(0, prefix.length(), prefix) == 0;
   out->assign(has_prefix ? in.substr(prefix.length()) : in);
@@ -82,214 +71,56 @@ bool TryStripPrefixString(const string& in, const string& prefix, string* out) {
   return has_prefix;
 }
 
-bool HasSuffixString(const string& s, const string& suffix) {
+bool HasSuffixString(const string &s, const string &suffix) {
   return absl::EndsWith(s, suffix);
 }
 
 template <typename T>
-void GenericAtoi(const string& s, T* out) {
-  if (!absl::SimpleAtoi(s, out))
-    *out = 0;
+void GenericAtoi(const string &s, T *out) {
+  if (!absl::SimpleAtoi(s, out)) *out = 0;
 }
 
-void safe_strto32(const string& s, int32 *n) {
-  GenericAtoi(s, n);
+void safe_strto32(const string &s, int32 *n) { GenericAtoi(s, n); }
+
+void safe_strtou64(const string &s, uint64 *n) { GenericAtoi(s, n); }
+
+void safe_strto64(const string &s, int64 *n) { GenericAtoi(s, n); }
+
+void strrmm(string *s, const string &chars) {
+  s->erase(std::remove_if(s->begin(), s->end(),
+                          [&chars](char current_char) {
+                            return chars.find(current_char) !=
+                                   std::string::npos;
+                          }),
+           s->end());
 }
 
-void safe_strtou64(const string& s, uint64 *n) {
-  GenericAtoi(s, n);
-}
-
-void safe_strto64(const string& s, int64* n) {
-  GenericAtoi(s, n);
-}
-
-void strrmm(string* s, const string& chars) {
-  for (string::iterator it = s->begin(); it != s->end(); ) {
-    const char current_char = *it;
-    if (chars.find(current_char) != string::npos) {
-      it = s->erase(it);
-    } else {
-      ++it;
-    }
-  }
-}
-
-int GlobalReplaceSubstring(const string& substring,
-                           const string& replacement,
-                           string* s) {
-  return absl::StrReplaceAll({{substring, replacement}}, s);;
+int GlobalReplaceSubstring(const string &substring, const string &replacement,
+                           string *s) {
+  return absl::StrReplaceAll({{substring, replacement}}, s);
+  ;
 }
 
 // StringHolder class
-StringHolder::StringHolder(const string& s)
-  : absl::AlphaNum(s)
-{}
+StringHolder::StringHolder(const string &s) : absl::AlphaNum(s) {}
 
-StringHolder::StringHolder(const char* cp)
-  : absl::AlphaNum(cp)
-{}
+StringHolder::StringHolder(const char *cp) : absl::AlphaNum(cp) {}
 
-StringHolder::StringHolder(uint64 n)
-  : absl::AlphaNum(n)
-{}
-
-StringHolder::~StringHolder() {}
+StringHolder::StringHolder(uint64 n) : absl::AlphaNum(n) {}
 
 // StrCat
 
 // Implements s += sh; (s: string, sh: StringHolder)
-string& operator+=(string& lhs, const StringHolder& rhs) {
-  absl::string_view s = rhs.GetString();;
-  if (s.size() != 0) {
+string &operator+=(string &lhs, const StringHolder &rhs) {
+  absl::string_view s = rhs.GetString();
+  ;
+  if (!s.empty()) {
     lhs += s.data();
   } else {
-    const char* const cs = rhs.GetCString();
-    if (cs)
-      lhs.append(cs, rhs.Length());
+    const char *const cs = rhs.GetCString();
+    if (cs) lhs.append(cs, rhs.Length());
   }
   return lhs;
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2) {
-  return absl::StrCat(s1, s2);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3) {
-  return absl::StrCat(s1, s2, s3);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4) {
-  return absl::StrCat(s1, s2, s3, s4);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5) {
-  return absl::StrCat(s1, s2, s3, s4, s5);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5, const StringHolder& s6) {
-  return absl::StrCat(s1, s2, s3, s4, s5, s6);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5, const StringHolder& s6,
-              const StringHolder& s7) {
-  return absl::StrCat(s1, s2, s3, s4, s5, s6, s7);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5, const StringHolder& s6,
-              const StringHolder& s7, const StringHolder& s8) {
-  string result;
-  result.reserve(s1.Length() + s2.Length() + s3.Length() + s4.Length() +
-                 s5.Length() + s6.Length() + s7.Length() + s8.Length() + 1);
-  return absl::StrCat(s1, s2, s3, s4, s5, s6, s7, s8);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5, const StringHolder& s6,
-              const StringHolder& s7, const StringHolder& s8,
-              const StringHolder& s9) {
-  return absl::StrCat(s1, s2, s3, s4, s5, s6, s7, s8, s9);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5, const StringHolder& s6,
-              const StringHolder& s7, const StringHolder& s8,
-              const StringHolder& s9, const StringHolder& s10,
-              const StringHolder& s11) {
-  return absl::StrCat(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5, const StringHolder& s6,
-              const StringHolder& s7, const StringHolder& s8,
-              const StringHolder& s9, const StringHolder& s10,
-              const StringHolder& s11, const StringHolder& s12) {
-  return absl::StrCat(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5, const StringHolder& s6,
-              const StringHolder& s7, const StringHolder& s8,
-              const StringHolder& s9, const StringHolder& s10,
-              const StringHolder& s11, const StringHolder& s12,
-              const StringHolder& s13) {
-  return absl::StrCat(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12,
-                      s13);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5, const StringHolder& s6,
-              const StringHolder& s7, const StringHolder& s8,
-              const StringHolder& s9, const StringHolder& s10,
-              const StringHolder& s11, const StringHolder& s12,
-              const StringHolder& s13, const StringHolder& s14) {
-  return absl::StrCat(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12,
-                      s13, s14);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5, const StringHolder& s6,
-              const StringHolder& s7, const StringHolder& s8,
-              const StringHolder& s9, const StringHolder& s10,
-              const StringHolder& s11, const StringHolder& s12,
-              const StringHolder& s13, const StringHolder& s14,
-              const StringHolder& s15) {
-  return absl::StrCat(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12,
-                      s13, s14, s15);
-}
-
-string StrCat(const StringHolder& s1, const StringHolder& s2,
-              const StringHolder& s3, const StringHolder& s4,
-              const StringHolder& s5, const StringHolder& s6,
-              const StringHolder& s7, const StringHolder& s8,
-              const StringHolder& s9, const StringHolder& s10,
-              const StringHolder& s11, const StringHolder& s12,
-              const StringHolder& s13, const StringHolder& s14,
-              const StringHolder& s15, const StringHolder& s16) {
-  return absl::StrCat(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12,
-                      s13, s14, s15, s16);
-}
-
-// StrAppend
-
-void StrAppend(string* dest, const StringHolder& s1) {
-  absl::StrAppend(dest, s1);
-}
-
-void StrAppend(string* dest, const StringHolder& s1, const StringHolder& s2) {
-  absl::StrAppend(dest, s1, s2);
-}
-
-void StrAppend(string* dest, const StringHolder& s1, const StringHolder& s2,
-               const StringHolder& s3) {
-  absl::StrAppend(dest, s1, s2, s3);
-}
-
-void StrAppend(string* dest, const StringHolder& s1, const StringHolder& s2,
-               const StringHolder& s3, const StringHolder& s4) {
-  absl::StrAppend(dest, s1, s2, s3, s4);
-}
-
-void StrAppend(string* dest, const StringHolder& s1, const StringHolder& s2,
-               const StringHolder& s3, const StringHolder& s4,
-               const StringHolder& s5) {
-  absl::StrAppend(dest, s1, s2, s3, s4, s5);
 }
 
 }  // namespace phonenumbers
