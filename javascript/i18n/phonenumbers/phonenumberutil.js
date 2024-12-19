83873036dac4f470e8a7321aa7536240b1c72725
SG-Kang <101150768+SG-Kang@users.noreply.github.com>
2024-12-19 14:42:59 +0900

    Add case insensitive option for regexp

--- javascript/i18n/phonenumbers/phonenumberutil.js
+++ javascript/i18n/phonenumbers/phonenumberutil.js
@@ -4759,7 +4759,7 @@ i18n.phonenumbers.PhoneNumberUtil.prototype.canBeInternationallyDialled =
  */
 i18n.phonenumbers.PhoneNumberUtil.matchesEntirely = function(regex, str) {
   /** @type {Array.<string>} */
-  var matchedGroups = str.match('^(?:' + (typeof regex == 'string' ? regex : regex.source) + ')$')
+  var matchedGroups = str.match(new RegExp('^(?:' + (typeof regex == 'string' ? regex : regex.source) + ')$', 'i'));
   if (matchedGroups && matchedGroups[0].length == str.length) {
     return true;
   }
